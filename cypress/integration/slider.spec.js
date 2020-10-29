f01e5cc916fec96a8939009d4e01525a6c28cd6a
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-10-29 14:34:37 +0100

    Add "Slider" test

---
+++ cypress/integration/slider.spec.js
@@ -0,0 +1,9 @@
+describe('Slider', () => {
+  it('should pick a value on slider', () => {
+    cy.visit('https://devexpress.github.io/testcafe/example/');
+
+    cy.get('#tried-test-cafe').click();
+
+    cy.contains('7').click();
+  });
+});
